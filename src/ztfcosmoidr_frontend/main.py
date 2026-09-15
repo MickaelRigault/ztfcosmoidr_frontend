c376cea9c29cde11f5a940b299ca832234e03093
MickaelRigault <mick.rigault@gmail.com>
2026-09-15 13:41:14 -0400

    define super user

--- src/ztfcosmoidr_frontend/main.py
+++ src/ztfcosmoidr_frontend/main.py
@@ -47,7 +47,7 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'  # where to point to log if needed
 
 
-SUPER_USER = []
+SUPER_USER = ["burgaz.umut@gmail.com", "terwelj@tcd.ie"]
 
 class Classifications(release_db.Model):
     id = release_db.Column(release_db.Integer, primary_key=True)
